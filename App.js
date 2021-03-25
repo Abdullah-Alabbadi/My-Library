45be6de7d62b1a0665c4a995c90144f621c4776e
Abdullah Alabbadi <abdullah_alabbadi2018@yahoo.com>
2021-03-25 16:21:42 +0200

    new css

---
+++ App.js
@@ -0,0 +1,26 @@
+alert("Welcome to your Dashboard")
+
+
+var fullName = firstName + lastName;
+var firstName ='Abdullah ';
+var lastName  ='Alabbadi';
+
+var fullName = prompt('What is your Full name');
+var txt;
+
+
+if (fullName == firstName + lastName ){
+
+    
+    var fullName = confirm("do you want to go to your Dashboard?");
+    alert("Welcome to your Dashboard");
+}else if(fullName == true){txt = "You pressed OK!";}
+
+else {
+
+        alert("You must take permission to enter");
+        
+       }  
+     
+    
+     
