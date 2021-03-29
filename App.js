017ce1f374124d60252324dfb3b7888c4a27b106
Abdullah Alabbadi <abdullah_alabbadi2018@yahoo.com>
2021-03-29 17:47:22 +0300

    the library was more colorful

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-alert("Welcome to your Dashboard")
+ alert("Welcome to your Dashboard")
 
 var txt;
 var firstName ='Abdullah ';
@@ -7,8 +7,8 @@ var lastName  ='Alabbadi';
 
 
 
-
-/* Add validation on one of the user inputs.
+/*
+ Add validation on one of the user inputs.
 Keep asking until an acceptable value is entered.
 Show an image related to your page’s topic, repeated within your page.
 Prompt the user with a relevant question for a number (e.g.: “How many cats do you want?” or “What star rating would you give, 1-5?”)
@@ -57,6 +57,7 @@ while(favInstrument !== 'Software Enginering' && favInstrument !== 'java3'){
 
 var instrumentsNum = prompt('how many time to see the book');
 
+function bookPhoto(){
 var image = '';
 for(var i = 1 ; i <=instrumentsNum; i++ ){
     if(favInstrument === 'java3'){
@@ -67,3 +68,6 @@ for(var i = 1 ; i <=instrumentsNum; i++ ){
 }
      
 document.write(image);
+
+}
+bookPhoto(); 
